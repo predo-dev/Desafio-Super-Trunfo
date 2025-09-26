1e87915e29f9da1c98f7b6fd1df0c5f1172e0b04
predo-dev <predodev@gmail.com>
2025-09-25 21:25:03 -0300

    Create nivel_novato.c

---
+++ nivel_novato.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+
+
+int main(){
+    char estado_1[3] , estado_2[3] , cidade_1[20] , cidade_2[20],
+    codigo_1[5] , codigo_2[5];
+    int populacao_1, populacao_2 , ponto_turisticos_1 , pontos_turisticos_2;
+    float area_1 , area_2 , pib_1 , pib_2;
+    printf("Adicionando Carta 1:\n");
+    printf("Estado: \n");
+    scanf("%s" , estado_1);
+    printf("Código da carta: \n");
+    scanf("%s" , codigo_1);
+    printf("Cidade: \n");
+    scanf("%s" , cidade_1);
+    printf("Polpulação: \n");
+    scanf("%d" , &populacao_1);
+    printf("Área: \n");
+    scanf("%f" , &area_1);
+    printf("PIB: \n");
+    scanf("%f" , &pib_1);
+    printf("Número de pontos turisticos\n");
+    scanf("%d" , &ponto_turisticos_1);
+
+    printf("Carta 1 foi adicionada.\n");
+    printf("\n");
+    printf("Adicionando carta 2\n");
+    printf("Estado: \n");
+    scanf("%s" , estado_2);
+    printf("Código da carta: \n");
+    scanf("%s" , codigo_2);
+    printf("Cidade: \n");
+    scanf("%s" , cidade_2);
+    printf("Polpulação: \n");
+    scanf("%d" , &populacao_2);
+    printf("Área: \n");
+    scanf("%f" , &area_2);
+    printf("PIB: \n");
+    scanf("%f" , &pib_2);
+    printf("Número de pontos turisticos\n");
+    scanf("%d" , &pontos_turisticos_2);
+
+    printf("Carta 1:\n");
+    printf("Estado:%s\n" ,estado_1);
+    printf("Código da carta:%s\n",codigo_1);
+    printf("Cidade:%s\n",cidade_1); 
+    printf("População:%d\n",populacao_1);
+    printf("Aréa:%.2fkm\n",area_1);
+    printf("Pib:R$%.2f\n",pib_1);
+    printf("Pontos Turísticos:%d\n",ponto_turisticos_1);
+    printf("\n");
+    
+    printf("Carta 2:\n");
+    printf("Estado:%s\n" ,estado_2);
+    printf("Código da carta:%s\n",codigo_2);
+    printf("Cidade:%s\n",cidade_2); 
+    printf("População:%d\n",populacao_2);
+    printf("Aréa:%.2fkm\n",area_2);
+    printf("Pib:R$%.2f\n",pib_2);
+    printf("Pontos Turísticos:%d\n",pontos_turisticos_2);
+    
+    return 0;
+}
